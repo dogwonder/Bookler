a8c34a8b6975430fd8abbf188f6a166ba5ab18f6
Rich Holman <dogwonder@gmail.com>
2015-02-10 18:40:29 +0000

    tidying up

--- booker.rb
+++ booker.rb
@@ -14,7 +14,7 @@ post '/' do
    feed = FeedNormalizer::FeedNormalizer.parse open(feedurl)
    feed.entries.each do |post|
       source = open(post.urls.first).read
-      rtext = Readability::Document.new(source, :tags => %w[div p img a], :attributes => %w[src href], :remove_empty_nodes => false).content;
+      rtext = Readability::Document.new(source, :tags => %w[p img], :attributes => %w[src href], :remove_empty_nodes => false).content;
       @chapters.push("title"=>post.title,"content"=>rtext)
 
   end
